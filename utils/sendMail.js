41b51b1dd20ca2efa4a350c6c08115198d21f545
Aniket <aniketpatil0003072004@gmail.com>
2025-09-16 21:07:26 +0530

    sendgrid

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,28 +1,21 @@
 const nodemailer = require("nodemailer")
 
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SEND_GRID_KEY)
-
 const sendMail = async (message, to) => {
 
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth: {
+        user: "proctor.ai.main@gmail.com",
+        pass: process.env.GMAIL_APP_KEY,
+    },
+})
 
-
-const msg = {
-  to: to, 
-  from: 'aniketpatil032004@gmail.com', 
-  subject: 'Entry Token Proctor AI',
-  text: message,
-  html: `<strong>${message}</strong>`,
-}
-
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+await transporter.sendMail({
+    from: `ProctorAI`,
+    to: to,
+    subject: "Entry Token Proctor AI",
+    text: message
+  });
 
 
 }
